0500029398342995934aaed1aed4c0ea7dcf9116
Chujie Chen <cchen641@gatech.edu>
2020-11-02 21:41:46 -0800

    add a post to test

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.post('/', function (req, res) {
+    res.send("This is a post request");
+});
 
 app.listen(process.env.PORT || 3000);
