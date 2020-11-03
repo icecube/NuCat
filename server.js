e05efc0c54df7d49bbf42fbbcadc044ccc2d98dc
Chujie Chen <cchen641@gatech.edu>
2020-11-02 20:57:11 -0800

    try static

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-const app = require('./src/index');
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
 
-http.createServer(app).listen(process.env.PORT || 3000);
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
