c70698ab949344342eec8fc998e8b2f22ac0978b
Chujie Chen <cchen641@gatech.edu>
2020-11-04 23:03:35 -0800

    deploy

--- server.js
+++ server.js
@@ -13,33 +13,22 @@ const db = require('./server/db')
 const eventRouter = require('./server/routes/event-router')
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 app.use(bodyParser.json())
 
-app.use(express.static(path.join(__dirname, 'build')));
-
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
-// mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.DB_PASS}@nucatgt.jfmfo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false
-// });
-// mongoose.set("useCreateIndex", true)
-// const connection = mongoose.connection;
-// connection.once("open", function () {
-//     console.log("Connection with MongoDB was successful");
-// });
 
 // app.get('/', function (req, res) {
 //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
 
-app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+
 
 // app.post('/', function (req, res) {
 //     res.send("This is a post request");
