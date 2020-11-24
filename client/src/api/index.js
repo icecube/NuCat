1f0aaedb4253f2eb2e0fb22efa5043ae3e0d597a
Chujie Chen <cchen641@gatech.edu>
2020-11-23 20:30:40 -0800

    install packages

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,28 +1,29 @@
 // require('dotenv').config()
 import axios from 'axios'
 
-const api = axios.create({
-    // baseURL: 'https://cchen.nucat.gatech.edu/api',
-    baseURL: 'http://localhost:3000/api',
-})
+// const api = axios.create({
+//     // baseURL: 'https://cchen.nucat.gatech.edu/api',
+//     baseURL: 'http://localhost:3000/api',
+// })
+const api = axios
 
-export const insertEvent = payload => api.post(`/event`, payload)
-export const getAllEvents = () => api.get(`/events`)
-export const updateEventById = (id, payload) => api.put(`/event/${id}`, payload)
-export const deleteEventById = id => api.delete(`/event/${id}`)
-export const getEventById = id => api.get(`/event/${id}`)
+export const insertEvent = payload => api.post(`/api/event`, payload)
+export const getAllEvents = () => api.get(`/api/events`)
+export const updateEventById = (id, payload) => api.put(`/api/event/${id}`, payload)
+export const deleteEventById = id => api.delete(`/api/event/${id}`)
+export const getEventById = id => api.get(`/api/event/${id}`)
 
 // export const insertInfo = payload => api.post(`/info`, payload)
-export const getAllInfos = () => api.get(`/infos`)
+export const getAllInfos = () => api.get(`/api/infos`)
 // export const updateInfoById = (id, payload) => api.put(`/info/${id}`, payload)
 // export const deleteInfoById = id => api.delete(`/info/${id}`)
-export const getInfoById = id => api.get(`/info/${id}`)
+export const getInfoById = id => api.get(`/api/info/${id}`)
 
 // export const insertCandidate = payload => api.post(`/candidate`, payload)
-export const getAllCandidates = () => api.get(`/candidates`)
+export const getAllCandidates = () => api.get(`/api/candidates`)
 // export const updateCandidateById = (id, payload) => api.put(`/candidate/${id}`, payload)
 // export const deleteCandidateById = id => api.delete(`/candidate/${id}`)
-export const getCandidateById = id => api.get(`/candidate/${id}`)
+export const getCandidateById = id => api.get(`/api/candidate/${id}`)
 
 const apis = {
     insertEvent,
--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ const db = require('./db')
 const eventRouter = require('./routes/event-router')
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+const CLIENT_BUILD_PATH = path.join(__dirname, "../client/build");
+app.use(express.static(CLIENT_BUILD_PATH));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 app.use(bodyParser.json())
@@ -21,10 +22,10 @@ app.use(bodyParser.json())
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/app', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
 });
 app.get('/app/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
 });
 
 app.get('/', function (req, res) {
