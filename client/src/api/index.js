33fb43bf2b548ad07d35b1166fd97c8cdf38c7cb
Chujie Chen <cchen641@gatech.edu>
2020-11-24 14:54:08 -0800

    finish Dockerization Dev

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 // const api = axios.create({
 //     // baseURL: 'https://cchen.nucat.gatech.edu/api',
-//     baseURL: 'http://localhost:3000/api',
+//     baseURL: 'http://localhost:3000/api',  // backend running on 3000
 // })
 const api = axios
 
