ecb0b2060e7efdc72c55810da76c3381fd35af20
AdvaitDhingra <advait.dhingra1006@gmail.com>
2021-04-06 17:02:12 +0200

    Improved Navbar style and fixed port selection on Windows 10

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
+import Appbar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+
 import Logo from './Logo'
 import Links from './Links'
 
-const Container = styled.div.attrs({
-    className: 'container-fluid',
-})``
-
 const Nav = styled.nav.attrs({
-    className: 'navbar navbar-expand-lg navbar-dark bg-dark',
+    className: 'navbar navbar-expand-lg navbar-dark',
 })`
     margin-bottom: 20 px;
 `
@@ -17,14 +16,25 @@ const Nav = styled.nav.attrs({
 class NavBar extends Component {
     render() {
         return (
-            <Container>
-                <Nav>
-                    <Logo />
-                    <Links />
-                </Nav>
-            </Container>
+            <Appbar position = "static" style = {{
+                backgroundColor: "gray"
+            }}>
+                <Toolbar >
+                    <Logo/>
+                    <Nav><Links/></Nav>
+                    
+                </Toolbar>
+            </Appbar>
         )
     }
 }
 
 export default NavBar
+/*
+<Container>
+                <Nav>
+                    <Logo />
+                    <Links />
+                </Nav>
+            </Container>
+*/
