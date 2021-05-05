b6cd039d7f1c046700022f4caf0ff2b034024a19
David Schultz <davids24@gmail.com>
2021-05-05 13:04:02 -0500

    changes to connect to mongo at Madison, but preserve old semantics if not specified (#6)

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,17 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
+let db_url = 'mongodb'
+if (process.env.DB_SRV != "false") {
+    db_url += '+srv'
+}
+let db_url_log = db_url+`://XXXX:XXXX@${process.env.DB_ADDRESS}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+db_url += `://${process.env.DB_USERNAME}:${process.env.DB_PASS}@${process.env.DB_ADDRESS}/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+console.log("Connecting to MongoDB via "+db_url_log)
+
 mongoose
-    .connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@${process.env.DB_ADDRESS}/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
+    .connect(db_url, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false
